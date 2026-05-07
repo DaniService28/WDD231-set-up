b8fe1fc04cf1b85982dba896b7a385d03d89333c
DaniService28 <her23048@byui.edu>
2026-05-07 14:43:47 -0600

    img

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ function loadParkData() {
   document.getElementById("parkName").textContent = "Yellowstone";
   document.getElementById("parkType").textContent = "National Park";
   document.getElementById("parkStates").textContent = "WY, ID, MT";
-  document.querySelector("#park-image").src = "/images/yellowstone.jpg";
+  document.querySelector("#park-image").src = "images/yellowstone.jpg";
 }
 
 loadParkData();
